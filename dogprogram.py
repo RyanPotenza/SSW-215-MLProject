61c05013f5c522027c212899fd812088917e78f2
Ryan Potenza <rpotenza@stevens.edu>
2022-11-28 20:59:01 -0500

    Testing loading labels

--- dogprogram.py
+++ dogprogram.py
@@ -1,5 +1,14 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
-train_ds, test_ds = tfds.load('stanford_dogs', split=['train', 'test[:10%]'])
+import numpy as np
+import matplotlib.pyplot as plt
 
+# train_ds, test_ds = tfds.load('stanford_dogs', split=['train', 'test[:10%]'])
+dataset = tfds.builder('stanford_dogs')
+info = dataset.info
+info.features
+class_names = []
+for i in range(info.features["label"].num_classes):
+    class_names.append(info.features["label"].int2str(i))
+class_names
