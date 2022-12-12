26076fd16561c25d0464dc1e1ab894f48fcf53ec
Ryan Potenza <rpotenza@stevens.edu>
2022-12-11 20:57:43 -0500

    Update dogprogram.py

--- dogprogram.py
+++ dogprogram.py
@@ -4,11 +4,12 @@ import tensorflow_datasets as tfds
 import numpy as np
 import matplotlib.pyplot as plt
 
-# train_ds, test_ds = tfds.load('stanford_dogs', split=['train', 'test[:10%]'])
-dataset = tfds.builder('stanford_dogs')
-info = dataset.info
-info.features
-class_names = []
-for i in range(info.features["label"].num_classes):
-    class_names.append(info.features["label"].int2str(i))
-class_names
+dataset, dsinfo = tfds.load('stanford_dogs', with_info=True)
+label_name = dsinfo.features['label'].int2str
+
+for i in dataset['train'].take(10):
+    plt.figure()
+    plt.imshow(i['image'])
+    plt.title(label_name(i['label']))
+    
+    
